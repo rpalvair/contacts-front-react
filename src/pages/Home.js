00726d017e328e64abaa81b82f7c06a2d595971b
ruddy palvair <r.palvair@gmail.com>
2021-04-22 13:57:39 +0200

    feat : Load contacts on home page feat : Display icons feat : Add router link on button

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,7 +1,62 @@
-import React from "react"
+import React, { Component } from "react"
+import config from "../config"
+import { Card, Container, Row, Col } from "react-bootstrap"
+import "./Home.css"
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faTrash, faEdit } from '@fortawesome/free-solid-svg-icons'
+import { Link } from "react-router-dom"
 
-const Home = () => {
-    return <div>Welcome home</div>
+class Home extends Component {
+
+    state = {
+        contacts: []
+    }
+
+    componentDidMount = () => {
+        console.log("component is mounted")
+        fetch(config.endpoints.contacts.read)
+            .then(response => {
+                console.log("response", response)
+                response.json()
+                    .then(data => {
+                        console.log("data", data)
+                        this.setState({
+                            contacts: data
+                        })
+                    })
+            })
+    }
+
+    render() {
+        const { contacts } = this.state
+        return <Container>
+            <Row className="justify-content-md-center">
+                <Col lg="3">
+                    <div className="buttons">
+                        <Link className="btn btn-primary" to="/add-contact">
+                            Add new contact
+                        </Link>
+                    </div>
+                </Col>
+                <Col lg="6" of>
+                    {contacts.map(contact =>
+                        <Card key={contact.id} style={{ width: '18rem' }}>
+                            <Card.Body>
+                                <Card.Title>{contact.firstName} {contact.lastName}</Card.Title>
+                                <Card.Subtitle className="mb-2 text-muted">{contact.age} years old</Card.Subtitle>
+                                <div class="actions">
+                                    <FontAwesomeIcon className="icon" icon={faTrash} />
+                                    <FontAwesomeIcon className="icon" icon={faEdit} />
+                                </div>
+
+                            </Card.Body>
+                        </Card>
+                    )
+                    }
+                </Col>
+            </Row>
+        </Container>
+    }
 }
 
 export default Home
